b7de2c967f1725d0026e393cbf841d4c5a3f9e3c
Edmark C. Talingting <145763529+edmark1434@users.noreply.github.com>
2025-04-12 03:10:05 +0800

    Update App.js

--- frontend/js/App.js
+++ frontend/js/App.js
@@ -12,19 +12,36 @@ createApp({
       ShowRequestDetails: false,
       ShowAdmissionForm: false,
 
-      // Form fields
       firstName: '',
+      firstNameReq: '',
       middleName: '',
       lastName: '',
+      lastNameReq: '',
       extension: '',
       dateOfBirth: '',
       lrn: '',
       homeAddress: '',
       email: '',
+      emailReq: '',
       documentType: '',
       purpose: '',
       trackID: '',
-
+      lrnMessage: 'This field is required.',
+      
+      //fields Validation Admission
+      firstNameField: false,
+      lastNameField: false,
+      dateOfBirthField: false,
+      lrnField: false,
+      homeAddressField: false,
+      emailField: false,
+      currentDate: new Date().toISOString().split('T')[0],
+      
+      //fields Validation Request
+      firstNameReqField: false,
+      lastNameReqField: false,
+      emailReqField: false,
+      purposeReqField: false,
 
       // Validation
       submitted: false,
@@ -60,14 +77,19 @@ createApp({
     },
     submitRequest() {
       this.submitted = true;
+      this.firstNameReqField = this.firstNameReq ? true : false;
+      this.lastNameReqField = this.lastNameReq ? true : false;
+      this.emailReqField = (this.emailReq && this.isEmailValid(this.emailReq)) ? true : false;
+      this.purposeReqField = this.purposeReq ? true : false;
 
       if (
-        this.firstName &&
-        this.lastName &&
+        this.firstNameReqField &&
+        this.lastNameReqField &&
         this.documentType &&
-        this.purpose &&
-        this.lrn &&
-        (this.email === '' || this.isEmailValid())
+        this.purposeReqField &&
+        this.emailReqField &&
+        this.isEmailValid(this.emailReq)
+
       ) {
         alert('Request submitted successfully!');
         this.resetScreens();
@@ -104,28 +126,45 @@ createApp({
       this.ShowAdmissionForm = false;
     },
 
-    isEmailValid() {
-      return (
-        this.email === '' ||
-        /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(this.email)
-      );
+    isEmailValid(email) {
+      return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
     },
-
+    isNumeric(event) {
+      this.lrn = event.target.value.replace(/\D/g, '').slice(0, 12);
+    },
+    isLrnValid() {
+      if (this.lrn && this.lrn.length !== 12) {
+        this.lrnField = false;
+        this.lrnMessage = "Please enter a valid lrn";
+      }
+    },
     validateAdmissionForm() {
       this.submitted = true;
 
+      this.firstNameField = this.firstName ? true : false;
+      this.lastNameField = this.lastName ? true : false;
+      this.dateOfBirthField = this.dateOfBirth ? true : false;
+      this.lrnField = this.lrn ? true : false;
+      this.homeAddressField = this.homeAddress ? true : false;
+      this.emailField = (this.email && this.isEmailValid(this.email)) ? true : false;
+      this.isLrnValid()
       if (
-        this.firstName &&
-        this.lastName &&
-        this.dateOfBirth &&
-        this.lrn &&
-        this.homeAddress &&
-        this.isEmailValid()
+        this.firstNameField &&
+        this.lastNameField &&
+        this.dateOfBirthField &&
+        this.lrnField &&
+        this.homeAddressField &&
+        this.emailField &&
+        this.isEmailValid(this.email)
       ) {
-        alert('Form submitted successfully!');
         this.resetScreens();
         this.ShowServiceSuccess = true;
-        // Do actual submit or switch screen
+        this.ShowAdmissionForm = false;
+        this.firstName = '';
+        this.middleName = '';
+        this.lastName = '';
+        this.extension = '';
+        this.email = '';
       }
     }
 
